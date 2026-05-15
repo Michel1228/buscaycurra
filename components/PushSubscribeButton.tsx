8c9ea97db4043e559753d7313054fcef944e7d01
Michel Batista <michelbatistagonzalez1992@gmail.com>
2026-05-15 20:45:13 +0200

    fix: type assertion for applicationServerKey VAPID buffer

--- components/PushSubscribeButton.tsx
+++ components/PushSubscribeButton.tsx
@@ -47,7 +47,7 @@ export default function PushSubscribeButton() {
       const reg = await navigator.serviceWorker.ready;
       const sub = await reg.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+        applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY) as unknown as BufferSource,
       });
 
       const supabase = getSupabaseBrowser();
